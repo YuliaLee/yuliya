28344b679b55195a6f89e2cfde62cdeed9ac6666
Ivan Gorbachev <gim.spb@yandex.ru>
2019-04-24 11:19:08 +0300

    Added results metrics

--- metrics/resultingchartofmetrics.cpp
+++ metrics/resultingchartofmetrics.cpp
@@ -15,6 +15,7 @@
 #include <QtCharts/QLegend>
 #include <QtCharts/QBarCategoryAxis>
 #include <QtCharts/QValueAxis>
+#include <QtCharts/QLineSeries>
 
 QT_CHARTS_USE_NAMESPACE
 
@@ -22,7 +23,8 @@ ResultingInternalMetricsWidget::ResultingInternalMetricsWidget (const QString &p
     : QWidget (parent)
 {
     if (RedmineInstance::instance ().projects ().contains (prjid) &&
-            RedmineInstance::instance ().loadIssues (prjid))
+            RedmineInstance::instance ().loadIssues (prjid) &&
+            RedmineInstance::instance ().loadAttachments ())
     {
         setWindowTitle (trUtf8 ("Resulting Charts of Internal Metrics"));
 
@@ -31,31 +33,173 @@ ResultingInternalMetricsWidget::ResultingInternalMetricsWidget (const QString &p
         setLayout (vl);
 
         {
-//            int A = RedmineInstance::instance ().metric1A (prjid);
+            QBarSeries *series = new QBarSeries ();
 
-//            QBarSet *set0 = new QBarSet (trUtf8 ("Количество разработанных задач для предотвращения некорректных действий - %1").arg (QString::number (A)));
-//            *set0 << A;
+            QValueAxis *axisY = new QValueAxis ();
+            //axisY->setRange (0, 1);
+            //axisY->setTickCount (20);
+            //axisY->setMin (0);
+            //axisY->setMax (1);
+            axisY->applyNiceNumbers ();
+            series->attachAxis (axisY);
 
-//            //int B = project->_incorrect_actions;
-//            int B = RedmineInstance::instance ().metric1B (prjid);
-//            QBarSet *set1 = new QBarSet (trUtf8 ("Планируемое количество некорректных действий - %1").arg (QString::number (B)));
-//            *set1 << B;
+            //        QLineSeries *lineseries = new QLineSeries();
+            //        lineseries->setName ("max");
+            //        lineseries->append (QPoint (1, 0));
+            //        lineseries->append (QPoint (1, 1));
 
-//            QBarSeries *series = new QBarSeries ();
-//            series->append (set0);
-//            series->append (set1);
+            //-- Fault removal
+            {
+                int A = RedmineInstance::instance ().metric4A (prjid);
+                int B = RedmineInstance::instance ().metric4B (prjid);
+                float X = 0;
+                if (B != 0)
+                    X = (float)A/(float)B;
+                else
+                    X = 1;
 
-//            QChart *chart = new QChart ();
-//            chart->addSeries (series);
-//            chart->setTitle (trUtf8 ("Способность к предотвращению некоррестных действий"));
-//            chart->setAnimationOptions (QChart::SeriesAnimations);
+                QBarSet *set = new QBarSet (trUtf8 ("Fault removal %1").arg (QString::number (X, 'f', 2)));
+                *set << X;
+                series->append (set);
+            }
 
-//            chart->legend ()->setVisible (true);
-//            chart->legend ()->setAlignment (Qt::AlignBottom);
+            //-- Test adequance
+            {
+                int A = RedmineInstance::instance ().metric5A (prjid);
+                int B = RedmineInstance::instance ().metric5B (prjid);
+                float X = 0;
+                if (B != 0)
+                    X = (float)A/(float)B;
+                else
+                    X = 1;
 
-//            QChartView *chartView = new QChartView(chart);
-//            chartView->setRenderHint(QPainter::Antialiasing);
-//            vl->addWidget (chartView);
+                QBarSet *set = new QBarSet (trUtf8 ("Test adequance %1").arg (QString::number (X, 'f', 2)));
+                *set << X;
+                series->append (set);
+            }
+
+            //-- Failure avoidance
+            {
+                int A = RedmineInstance::instance ().metric2A (prjid);
+                int B = RedmineInstance::instance ().metric2B (prjid);
+                float X = 0;
+                if (B != 0)
+                    X = (float)A/(float)B;
+                else
+                    X = 1;
+
+                QBarSet *set = new QBarSet (trUtf8 ("Failure avoidance %1").arg (QString::number (X, 'f', 2)));
+                *set << X;
+                series->append (set);
+            }
+
+            QChart *chart = new QChart ();
+            chart->addSeries (series);
+            //chart->addSeries (lineseries);
+            chart->addAxis (axisY, Qt::AlignLeft);
+            chart->setTitle (trUtf8 ("Resulting Charts of Internal Metrics"));
+            chart->setAnimationOptions (QChart::SeriesAnimations);
+
+            chart->legend ()->setVisible (true);
+            chart->legend ()->setAlignment (Qt::AlignBottom);
+
+            QChartView *chartView = new QChartView (chart);
+            chartView->setRenderHint(QPainter::Antialiasing);
+            vl->addWidget (chartView);
         }
+
+        //*******************************************
+
+        {
+            QBarSeries *series = new QBarSeries ();
+
+            QValueAxis *axisY = new QValueAxis ();
+            //axisY->setRange (0, 1);
+            //axisY->setTickCount (20);
+            //axisY->setMin (0);
+            //axisY->setMax (1);
+            axisY->applyNiceNumbers ();
+            series->attachAxis (axisY);
+
+            //        QLineSeries *lineseries = new QLineSeries();
+            //        lineseries->setName ("max");
+            //        lineseries->append (QPoint (1, 0));
+            //        lineseries->append (QPoint (1, 1));
+
+            //-- Fault detection
+            {
+                int A = RedmineInstance::instance ().metric3A (prjid);
+                int B = RedmineInstance::instance ().metric3B (prjid);
+                float X = 0;
+                if (B != 0)
+                    X = (float)A/(float)B;
+                else
+                    X = 1;
+
+                QBarSet *set = new QBarSet (trUtf8 ("Fault detection %1").arg (QString::number (X, 'f', 2)));
+                *set << X;
+                series->append (set);
+            }
+
+            //-- Incorrect operation avoidance
+            {
+                int A = RedmineInstance::instance ().metric1A (prjid);
+                int B = RedmineInstance::instance ().metric1B (prjid);
+                float X = 0;
+                if (B != 0)
+                    X = (float)A/(float)B;
+                else
+                    X = 1;
+
+                QBarSet *set = new QBarSet (trUtf8 ("Incorrect operation avoidance %1").arg (QString::number (X, 'f', 2)));
+                *set << X;
+                series->append (set);
+            }
+
+            //-- Restorability
+            {
+                int A = RedmineInstance::instance ().metric6A (prjid);
+                int B = RedmineInstance::instance ().metric6B (prjid);
+                float X = 0;
+                if (B != 0)
+                    X = (float)A/(float)B;
+                else
+                    X = 1;
+
+                QBarSet *set = new QBarSet (trUtf8 ("Restorability %1").arg (QString::number (X, 'f', 2)));
+                *set << X;
+                series->append (set);
+            }
+
+            //-- Reliability Compliance
+            {
+                int A = RedmineInstance::instance ().metric7A (prjid);
+                int B = RedmineInstance::instance ().metric7B (prjid);
+                float X = 0;
+                if (B != 0)
+                    X = (float)A/(float)B;
+                else
+                    X = 1;
+
+                QBarSet *set = new QBarSet (trUtf8 ("Reliability Compliance %1").arg (QString::number (X, 'f', 2)));
+                *set << X;
+                series->append (set);
+            }
+
+            QChart *chart = new QChart ();
+            chart->addSeries (series);
+            //chart->addSeries (lineseries);
+            chart->addAxis (axisY, Qt::AlignLeft);
+            chart->setTitle (trUtf8 ("Resulting Charts of Internal Metrics"));
+            chart->setAnimationOptions (QChart::SeriesAnimations);
+
+            chart->legend ()->setVisible (true);
+            chart->legend ()->setAlignment (Qt::AlignBottom);
+
+            QChartView *chartView = new QChartView (chart);
+            chartView->setRenderHint(QPainter::Antialiasing);
+            vl->addWidget (chartView);
+        }
+
     }
 }
